8b6c51ae586269a7cb0d3527d9923bb84f3f8f4b
Kenneth Tero <ktero.dev@gmail.com>
2023-05-22 22:48:32 +0800

    add test: Add first test

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -4,12 +4,16 @@ import { test, expect } from '@playwright/test'
 
 test.beforeEach( async ({page}) => {
   await page.goto('http://localhost:5173/')
-   await expect(page).toHaveURL(/localhost:5173/)
+  await expect(page).toHaveURL(/localhost:3000/)
 })
 
 test.describe("TODO app", () => {
   
-  test("Check URL", async ({page}) => {
-    await expect(page).toHaveURL(/localhost:5173/)
+  test("Add TODO item with title and description", async ({page}) => {
+    await expect(page).toHaveURL(/localhost:3000/)
+    await page.getByLabel('New Item').fill('This is the title');
+    await page.getByLabel('Add description').fill('This is the description');
+    await page.getByRole('button', { name: 'Add' }).click();
+    await page.getByText('TitleDescription').click();
   })
 })
