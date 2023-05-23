505e376a05bff747e72ecdab83b94c742b0cff6e
Kenneth Tero <ktero.dev@gmail.com>
2023-05-24 01:29:20 +0800

    add 2 new tests

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -8,12 +8,26 @@ test.beforeEach( async ({page}) => {
 
 test.describe("TODO app", () => {
   
-  test("Add TODO item with title and description", async ({page}) => {
+  test("Should add TODO item with title and description", async ({page}) => {
     await page.locator('[data-test="item"]').fill('This is the title')
     await page.locator('[data-test="description"]').fill('This is the description')
     await page.locator('[data-test="button-add-todo"]').click()
 
     await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the title')
     await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the description')
+  })
+
+  test("Should add TODO item with title only", async ({page}) => {
+    await page.locator('[data-test="item"]').fill('This is the title')
+    await page.locator('[data-test="button-add-todo"]').click()
+
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the title')
+  })
+
+  test("Should not add TODO item with description only", async ({page}) => {
+    await page.locator('[data-test="description"]').fill('This is the description')
+    await page.locator('[data-test="button-add-todo"]').click()
+
+    await expect(page.locator('[data-test="todo-list"]')).toHaveText('No Todos')
   })
 })
