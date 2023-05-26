7152940e28baa2883cb7415b0b2d87916a3fa820
Kenneth Tero <ktero.dev@gmail.com>
2023-05-26 19:30:29 +0800

    add webServer configuration and add base url

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -25,7 +16,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
@@ -69,10 +60,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000/',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000
+  }
 });
 
