0cfca825fb5ea2e9b73b3067859ff9d21642fcbd
Kenneth Tero <ktero.dev@gmail.com>
2023-05-27 00:08:21 +0800

    enhance test: add delete scenario

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -8,13 +8,16 @@ test.beforeEach( async ({page}) => {
 
 test.describe("TODO app", () => {
   
-  test("Should add TODO item with title and description", async ({page}) => {
+  test("Should add TODO item with title and description and should be able to delete the TODO item", async ({page}) => {
     await page.locator('[data-test="item"]').fill('This is the title')
     await page.locator('[data-test="description"]').fill('This is the description')
     await page.locator('[data-test="button-add-todo"]').click()
 
     await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the title')
     await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the description')
+
+    await page.locator('[data-test="button-delete"]').click()
+    await expect(page.locator('[data-test="todo-list"]')).toHaveText('No Todos')
   })
 
   test("Should add TODO item with title only", async ({page}) => {
