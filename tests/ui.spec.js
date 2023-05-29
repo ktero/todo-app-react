9649d502f59f1bab80148c4b5ea79ee6604bbf9b
Kenneth Tero <ktero.dev@gmail.com>
2023-05-30 00:06:12 +0800

    add new test as part of enhancement #14

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -33,4 +33,41 @@ test.describe("TODO app", () => {
 
     await expect(page.locator('[data-test="todo-list"]')).toHaveText('No Todos')
   })
+
+  test("Should edit TODO item after clicking save", async ({page}) => {
+    await page.locator('[data-test="item"]').fill('This is the title')
+    await page.locator('[data-test="description"]').fill('This is the description')
+    await page.locator('[data-test="button-add-todo"]').click()
+
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the title')
+    await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the description')
+
+    // Update both title and description
+    await page.locator('[data-test="button-edit"]').click()
+    await page.locator('[data-test="edit-item"]').fill('This is the updated title')
+    await page.locator('[data-test="edit-description"]').fill('This is the updated description')
+    await page.locator('[data-test="button-edit-save"]').click()
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the updated title')
+    await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the updated description')
+
+    // Update title only
+    await page.locator('[data-test="button-edit"]').click()
+    await page.locator('[data-test="edit-item"]').fill('This is the updated title the second')
+    await page.locator('[data-test="button-edit-save"]').click()
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the updated title the second')
+    await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the updated description')
+
+    // Update description only
+    await page.locator('[data-test="button-edit"]').click()
+    await page.locator('[data-test="edit-description"]').fill('This is the updated description the second')
+    await page.locator('[data-test="button-edit-save"]').click()
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the updated title the second')
+    await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the updated description the second')
+
+    // Update nothing and click save
+    await page.locator('[data-test="button-edit"]').click()
+    await page.locator('[data-test="button-edit-save"]').click()
+    await expect(page.locator('[data-test="todo-title"]')).toHaveText('This is the updated title the second')
+    await expect(page.locator('[data-test="todo-description"]')).toHaveText('This is the updated description the second')
+  })
 })
